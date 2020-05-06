419907bb7d31f7f199aa0d6928bce2459a081efd
Rodrigo C <rodrigoS16unicesumar@gmail.com>
2020-05-06 08:57:40 -0300

    Projeto iniciado, mostrando olá mundo

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace projectTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
